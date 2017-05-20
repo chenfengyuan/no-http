c2df7d9b77eef535bbb0ef43667f8624952f4648
Fengyuan Chen <cfy1990@gmail.com>
2017-05-20 15:11:30 +0800

    feat: dent http traffic

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from multiprocessing import Event
+#!/usr/bin/env python
+import re
 from signal import signal, SIGINT
 import asyncio
 import uvloop
 asyncio.set_event_loop(uvloop.new_event_loop())
+
+HTTP_RE = re.compile(rb'(?:User-Agent)|(?: HTTP/1.\d)')
 
 
 class EchoClientProtocol(asyncio.Protocol):
@@ -50,12 +53,13 @@ class EchoClientProtocol(asyncio.Protocol):
 
 
 class EchoServerClientProtocol(asyncio.Protocol):
-    def __init__(self):
+    def __init__(self, server_port):
         self.pair = None
         self.pair_coro = None
         self.pair_task = None
         self.closed = False
         self.peername = ''
+        self.server_port = server_port
 
     def connection_made(self, transport):
         peername = transport.get_extra_info('peername')
@@ -64,16 +68,20 @@ class EchoServerClientProtocol(asyncio.Protocol):
         self.transport = transport
         loop = asyncio.get_event_loop()
         self.pair = EchoClientProtocol(self)
-        self.pair_coro = loop.create_connection(lambda: self.pair, '127.0.0.1', 9000)
+        self.pair_coro = loop.create_connection(lambda: self.pair, '127.0.0.1', self.server_port)
         self.pair_task = asyncio.ensure_future(self.pair_coro)
+        self.pair_task.add_done_callback(self.pair_done_cb)
+
+    def pair_done_cb(self, future):
+        if future.exception():
+            print('server pair exc done: {}'.format(future.exception()))
+            self.pair_close()
 
     def data_received(self, data):
-        if self.pair_task:
-            exc = self.pair_task.exception()
-            if exc:
-                print('server: client connection exc: {}'.format(exc))
-                self.pair_close()
-                return
+        if HTTP_RE.search(data):
+            print('server: detect http request :{}'.format(data.decode('iso8859-1')))
+            self.transport.close()
+            return
         self.pair.send_data(data)
 
     def send_data(self, data):
@@ -96,8 +104,10 @@ class EchoServerClientProtocol(asyncio.Protocol):
 
 
 def main():
+    import sys
+    argv = sys.argv
     loop = asyncio.get_event_loop()
-    coro = loop.create_server(EchoServerClientProtocol, '127.0.0.1', 9001)
+    coro = loop.create_server(lambda: EchoServerClientProtocol(int(argv[2])), '127.0.0.1', int(argv[1]))
     signal(SIGINT, lambda s, f: loop.stop())
     asyncio.ensure_future(coro)
     try:
